7605c66f268ba3e1fd0d64cd5788cecbb873aa7f
Shetty-Aryan <shetty147aryan@gmail.com>
2026-02-19 20:53:33 +0530

    Added admin profile and made fixes to notification service

--- emergency-alert-app/src/firebase/firebaseConfig.ts
+++ emergency-alert-app/src/firebase/firebaseConfig.ts
@@ -2,25 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// 🔥 Firebase config (unchanged)
 const firebaseConfig = {
-  apiKey: "AIzaSyAXGiKGtJGfMVyYwu8gkXuP8dP0sy75VJY",
-  authDomain: "medride-5b2d9.firebaseapp.com",
-  projectId: "medride-5b2d9",
-  storageBucket: "medride-5b2d9.firebasestorage.app",
-  messagingSenderId: "24057609536",
-  appId: "1:24057609536:web:1331ea937dfe0da7ddcea5",
-
-
-  databaseURL: "https://medride-5b2d9-default-rtdb.asia-southeast1.firebasedatabase.app",
-
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ SAFE FOR EXPO (web + iOS + Android)
 export const auth = getAuth(app);
-
-// Realtime Database
 export const db = getDatabase(app);
